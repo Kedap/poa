848c63b7d906f6aedd4e7f208b8e7ee12d1af9fe
kedap <kedap.dev@protonmail.com>
2021-09-28 19:25:39 -0500

    update

--- install_apmpkg.sh
+++ install_apmpkg.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y file wget curl pkg-config libssl-dev git --no-install-recommends
-wget https://github.com/Kedap/apmpkg/releases/download/1.4.1/apmpkg-bin-x86_64
-chmod +x apmpkg-bin-x86_64
-mv apmpkg-bin-x86_64 /usr/bin/apmpkg
-git clone https://github.com/Kedap/apmpkg.git
-mkdir -p /etc/apmpkg/iiabc
-cp -r apmpkg/src/iiabc /etc/apmpkg
-rm -rf apmpkg
+apt-get install -y file wget curl pkg-config libssl-dev git zstd --no-install-recommends
+curl -o pacman_file.tar.zst https://krep0.bitbucket.io/archlinux/x86_64/apmpkg-dev-195.4e6c5c1-1-x86_64.pkg.tar.zst
+tar -xf pacman_file.tar.zst
+rm pacman_file.tar.zst
+cp -rf * /
+rm /usr/bin/apmpkg
+git clone https://github.com/kedap/apmpkg
+cd apmpkg
+cargo build --release --locked
+install -Dm 755 "target/release/apmpkg" -t "/usr/bin"
+cd ..
+rm -rf *
 rm -rf /var/lib/apt/lists/*
