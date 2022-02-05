cc7d14dada529f9c5afbc5a17bdbfcc08ffb6233
kedap <kedap.dev@protonmail.com>
2022-02-04 19:54:10 -0600

    actualizando dockerfile

--- install_rust.sh
+++ install_rust.sh
@@ -1,13 +1,9 @@
 #!/bin/sh -l
 
-echo "Downloading patch"
-curl -O https://krep0.bitbucket.io/pastes/abuild.conf.patch
-echo "Patching file"
-sudo patch -R -u -b /etc/abuild.conf -i abuild.conf.patch
 echo "Creating keys..."
 abuild-keygen -a -i -n
 echo "Adding comunity repository..."
-echo 'https://dl-cdn.alpinelinux.org/alpine/v3.14/community' >> /etc/apk/repositories
+echo 'https://dl-cdn.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
 echo "Installing rust..."
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "Load cargo path"
